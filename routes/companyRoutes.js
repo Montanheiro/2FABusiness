d8c50ba59a2b1da23fbc9e8de3c0e39902407ad1
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-09-09 02:04:15 -0300

    Fix name

--- routes/companyRoutes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    const controller = app.controllers.UserController;
-    const JWTPolicy = require('../lib/jwtVerify.js');
-
-    app.get('/user/get', JWTPolicy, controller.get);
-    app.post('/user/edit', JWTPolicy, controller.edit);
-    app.post('/user/changePassword', JWTPolicy, controller.changePassword);
-    app.post('/user/fieldVerify', controller.uniqueFieldVerify);
-}
