b62d9b7d6dc49b027559bf6385b2ad4d22b88c5d
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-09-09 02:20:18 -0300

    fix Business Model

--- models/BusinessModel.js
+++ models/BusinessModel.js
@@ -1,29 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
 
 let BusinessSchema = new Schema({
   name: require('./fields/required-field.js')('String','isName'),
-
-  paymentService:require('./fields/default-field.js')('String', null),
-  subscription:  require('./fields/field.js')('Mixed'),
-  transaction:  require('./fields/field.js')('Mixed'),
-
-  price:  require('./fields/required-default-field.js')('String', '0.00'),
 
   status: require('./fields/required-default-field.js')('Boolean', true)
 });
 
 BusinessSchema.plugin(require('./plugins/timestamp.js'));
-BusinessSchema.plugin(require('./plugins/passwordCriptografy'));
-
-BusinessSchema.methods.comparePassword = function (passw, cb) {
-    bcrypt.compare(passw, this.password, function (err, isMatch) {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, isMatch);
-    });
-};
 
 module.exports = mongoose.model('Business', BusinessSchema);
