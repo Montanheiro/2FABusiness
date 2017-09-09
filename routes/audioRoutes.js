60408df3ae919fc54870e57677d6eaa4480c3de8
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-09-09 01:57:57 -0300

    Removendo audio route

--- routes/audioRoutes.js
+++
@@ -1,8 +0,0 @@
-// module.exports = (app)=>{
-//   const controller = app.controllers.AudioController;
-//   const JWTPolicy = require('../lib/jwtVerify.js');
-
-//   app.post('/audio/new', JWTPolicy, controller.new);
-//   app.post('/audio/test', controller.test);
-
-// }
