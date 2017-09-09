71617aadba15e7b8951148ed5c5b3cae634a2054
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2017-09-09 01:52:25 -0300

    Removendo pagseguro

--- routes/pagseguroRoutes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app)=>{
-  const controller = app.controllers.PagseguroController;
-  const JWTPolicy = require('../lib/jwtVerify.js');
-
-  app.get('/pagseguro/subscription',JWTPolicy, controller.subscription);
-  app.get('/pagseguro/cancel', JWTPolicy, controller.cancel);
-  app.post('/pagseguro/notification',  controller.notification);
-  app.get('/pagseguro/getAll', JWTPolicy, controller.getAll);
-}
